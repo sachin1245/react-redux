f17a779becdc9ea0dccc650a5d56edbecac90656
Sachin C <sachin@springboard.com>
2019-10-28 04:36:00 -0700

    refactor redux to use standard react-redux packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import { updateCurrent } from './reducers/todo';
 
 function App(props) {
   return (
     <div className="App">
         <div className="Todo-App">
           <TodoForm 
-          changeCurrent={props.changeCurrent}
+          changeCurrent={props.updateCurrent}
           currentTodo={props.currentTodo} />
           <TodoList todos={props.todos}></TodoList>
         </div>
@@ -16,4 +18,7 @@ function App(props) {
   );
 }
 
-export default App;
+const mapDispatchToProps = {updateCurrent};
+const mapStateToProps = (state) => state;
+const connectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+export default connectedApp;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import { updateCurrent } from './reducers/todo';
 
-const todoChangeHandler = (val) => store.dispatch(
-    updateCurrent(val)
-);
+ReactDOM.render(
+    <Provider store={store}>
+        <App  />
+    </Provider>
+,
+    document.getElementById('root'));
 
-const render = () => {
-    const state = store.getState();
-    ReactDOM.render(<App 
-    todos={state.todos} 
-    changeCurrent={todoChangeHandler}
-    currentTodo={state.currentTodo} />,
-     document.getElementById('root'));
-}
 
-render();
-
-store.subscribe(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
